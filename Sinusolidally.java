3de6d002c41ab49dc0bc03dd36daca7f8336a1c3
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-26 19:54:24 -0400

    finished the full functionality of sinusolidally

--- Sinusolidally.java
+++ Sinusolidally.java
@@ -3,7 +3,7 @@ public class Sinusolidally {
 	// first iteration of the string. Grabs all of the high chars.
 	private static String highIteration(String sampleStr) {
 		String snakeStr = "";		
-		for (int i = 1; i < String.length(); i += 4) {
+		for (int i = 1; i < sampleStr.length(); i += 4) {
 			snakeStr += sampleStr.charAt(i);
 		}
 		return snakeStr;
@@ -11,7 +11,7 @@ public class Sinusolidally {
 
 	// second iteration for the middle characters of the string
 	private static String midIteration(String sampleStr, String currStr) {
-		for (int i = 0; i < String.length(); i += 2) {
+		for (int i = 0; i < sampleStr.length(); i += 2) {
 			currStr += sampleStr.charAt(i);
 		}
 		return currStr;
@@ -19,18 +19,19 @@ public class Sinusolidally {
 
 	// last iteration for the bottom characters of the string
 	private static String lowIteration(String sampleStr, String currStr) {
-		for (int i = 3; i < String.length(); i += 4) {
+		for (int i = 3; i < sampleStr.length(); i += 4) {
 			currStr += sampleStr.charAt(i);
 		}
 		return currStr;
 	}
 
 	// controlls all of the iterations
-	private static String snakeIt() {
-		
+	public static String snakeIt(String sampleStr) {
+		return lowIteration(sampleStr, (midIteration(sampleStr, highIteration(sampleStr))));
 	}
 
 	public static void main(String[] args) {
 
+		System.out.println(snakeIt(args[0]));
 	}
 }
