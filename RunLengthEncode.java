d94628c501d2668e0906495d8da49623c73658cf
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-26 21:59:07 -0400

    commented the RLE encode function

--- RunLengthEncode.java
+++ RunLengthEncode.java
@@ -3,16 +3,18 @@ public class RunLengthEncode {
 	// loops through the string and keeps track of the current pattern by testing the next indeex
 	public static String encode(String str) {
 		String code = "";
-		int reps = 1;
+		int reps = 1; // keeps track of the repitions of a char and always starts as 1 rep
 		for (int i = 0; i < str.length(); ++i) {
+			// for the purpose of not ending up out of bounds on the last rep
 			if (i < str.length() - 1) {
+				// comapres the forward char to the current char 
 				if (str.charAt(i) == str.charAt(i + 1)) {
 					++reps;
 				}
 				else {
 					code += reps;
 					code += str.charAt(i);
-					reps = 1;
+					reps = 1; // always starts as one rep
 				} 
 			}
 			else {
