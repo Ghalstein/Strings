20d25e9de9133090271b7957878e97ad59c38ba5
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-30 14:51:56 -0400

    made the base solution

---
+++ LongestSubstringWithNoReps.java
@@ -0,0 +1,24 @@
+class LongestSubstringWithNoReps {
+    
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> hash = new HashMap<>();
+        int count = 0;
+        int currCount = 0;
+        
+        for (int i = 0; i < s.length(); ++i) {
+           if (hash.containsKey(s.charAt(i))) {
+               if (currCount > count) count = currCount;
+               i = hash.get(s.charAt(i)) + 1;
+               hash.clear();
+               hash.put(s.charAt(i), i);
+               currCount = 1;
+           } 
+           else {
+              hash.put(s.charAt(i), i);
+              ++currCount;
+           }
+        }
+        if (currCount > count) return currCount;
+        return count;
+    }
+}
